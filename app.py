ed8a7832f1bd9c3d5ec16c3804f31c778b0392a1
isaac <isaacverm@proton.me>
2025-01-18 14:52:59 +0100

    serve post

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def show_index():
     return render_template('index.html')
+
+@app.route('/posts/<post_name>')
+def show_post(post_name):
+    # using f-string to avoid having to use the complete filename in the url
+    # now you don't have to specify the .html extension in the url yourself
+    # e.g. /post/first-post instead of /post/first-post.html
+    return send_from_directory('posts', f'{post_name}.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
