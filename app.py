1bbc90136bd6b92956eafc75cce72f2506ff05a2
IsaacVerm <isaacverm@gmail.com>
2025-02-11 22:03:40 +0100

    add post "Building a web app to help with reading a foreign language" (#4)
    * add screenshots post
    
    * add post in markdown and HTML format
    
    * add get_image_post function to serve post images
    
    * put each post in its own folder named after the post
    
    * remove get_image_post function
    
    * add post building web app foreign language
    
    ---------
    
    Co-authored-by: isaac <isaacverm@proton.me>

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def show_index():
 def show_post(post_name):
     # using f-string to avoid having to use the complete filename in the url
     # now you don't have to specify the .html extension in the url yourself
-    # e.g. /post/first-post instead of /post/first-post.html
+    # e.g. /posts/first-post instead of /posts/first-post.html
     return send_from_directory('posts', f'{post_name}.html')
