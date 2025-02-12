f18d2288a74286d648f2edef62fa55a619794f1e
IsaacVerm <isaacverm@gmail.com>
2025-02-12 14:07:42 +0100

    add posts overview page (#8)
    * update routing functions to use /static folder to serve HTML and images
    
    * add posts_overview page
    
    * move posts to /static/posts
    
    ---------
    
    Co-authored-by: isaac <isaacverm@proton.me>

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_index():
-    return render_template('index.html')
+    return send_from_directory('static/pages', 'posts_overview.html')
 
 @app.route('/posts/<post_name>')
 def show_post(post_name):
     # using f-string to avoid having to use the complete filename in the url
     # now you don't have to specify the .html extension in the url yourself
     # e.g. /posts/first-post instead of /posts/first-post.html
-    return send_from_directory('posts', f'{post_name}.html')
+    return send_from_directory('static/posts', f'{post_name}.html')
