a3fc4e0f1897233375ae9befad1eaa2fe8152c13
isaac <isaacverm@proton.me>
2025-02-12 14:43:18 +0100

    major parts (step, checklist, command, ...) are written in capital case

--- doc/how-to/add-post.py
+++ doc/how-to/add-post.py
@@ -2,7 +2,7 @@ import os
 
 def ask_for_post_name():
     os.system('clear') # if not the terminal will be cluttered with the previous steps
-    print("* step 0: what's the name of your post?")
+    print("* STEP 0: what's the name of your post?")
     print("")
     post_name_natural = input("Type the name of your post.")
     post_name_kebab_case = post_name_natural.lower().replace(" ", "-")
@@ -11,9 +11,9 @@ def ask_for_post_name():
 
 def create_post_in_obsidian(post_name_kebab_case):
     os.system('clear')
-    print("* step 1: create post in Obsidian")
+    print("* STEP 1: create post in Obsidian")
     print("")
-    print("checklist:")
+    print("CHECKLIST:")
     print("- post should be kept in posts folder")
     print("- there's a newline after each header")
     print("- there's a newline before starting a list")
@@ -24,9 +24,9 @@ def create_post_in_obsidian(post_name_kebab_case):
     
 def copy_obsidian_post_to_weblog_repo():
     os.system('clear')
-    print("* step 2: copy Obsidian post (and images used in the post) to weblog repo")
+    print("* STEP 2: copy Obsidian post (and images used in the post) to weblog repo")
     print("")
-    print("checklist:")
+    print("CHECKLIST:")
     print("- Markdown post should be copied to the posts folder")
     print("- images should be copied to the static/images/posts/{postNameDashes} folder")
     print("")
@@ -34,16 +34,16 @@ def copy_obsidian_post_to_weblog_repo():
     
 def convert_markdown_post_to_html():
     os.system('clear')
-    print("* step 3: convert Markdown post to html")
+    print("* STEP 3: convert Markdown post to html")
     print("")
     print("(assuming you're in the root of the weblog repo)")
-    print("command: pandoc posts/{postNameDashes}.md -o posts/{postNameDashes}.html")
+    print("COMMAND: pandoc posts/{postNameDashes}.md -o posts/{postNameDashes}.html")
     print("After running the command a HTML file of the Markdown post should be created in the posts folder.")
     input("Press Enter when you have converted the Markdown post to HTML...")
     
 def add_toc_to_html_post():
     os.system('clear')
-    print("* step 4: add table of contents to HTML post")
+    print("* STEP 4: add table of contents to HTML post")
     print("")
     print("open LibreChat")
     print("copy the HTML of the post into LibreChat")
@@ -54,12 +54,12 @@ def add_toc_to_html_post():
     
 def check_post_served_by_web_server():
     os.system('clear')
-    print("* step 5: run web server in Docker")
+    print("* STEP 5: run web server in Docker")
     print("")
-    print("command: docker build -t weblog .")
-    print("command: docker run -p 8000:8000 weblog")
+    print("COMMAND: docker build -t weblog .")
+    print("COMMAND: docker run -p 8000:8000 weblog")
     print("open http://0.0.0.0:8000/posts/{postNameDashes} in your browser")
-    print("checklist:")
+    print("CHECKLIST:")
     print("- post visible in the browser")
     print("- images displayed correctly")
     print("- table of contents displayed correctly")
