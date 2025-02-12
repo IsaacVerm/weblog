d11dff4e9fc4716400ff23e0926a0dbcf8d05113
isaac <isaacverm@proton.me>
2025-02-12 14:57:36 +0100

    use post_names variable created in ask_for_post_name in later steps and clean up layout

--- doc/how-to/add-post.py
+++ doc/how-to/add-post.py
@@ -4,8 +4,9 @@ def ask_for_post_name():
     os.system('clear') # if not the terminal will be cluttered with the previous steps
     print("* STEP 0: what's the name of your post?")
     print("")
-    post_name_natural = input("Type the name of your post.")
+    post_name_natural = input("Type the name of your post:\n\n")
     post_name_kebab_case = post_name_natural.lower().replace(" ", "-")
+    # dictionary with 2 versions because I'll definitely use the natural language version in this doc later on
     post_names = {'natural language': post_name_natural, 'kebab_case': post_name_kebab_case}
     return post_names
 
@@ -22,61 +23,67 @@ def create_post_in_obsidian(post_name_kebab_case):
     print("")
     input("Press Enter when you have created the post in Obsidian...")
     
-def copy_obsidian_post_to_weblog_repo():
+def copy_obsidian_post_to_weblog_repo(post_name_kebab_case):
     os.system('clear')
     print("* STEP 2: copy Obsidian post (and images used in the post) to weblog repo")
     print("")
     print("CHECKLIST:")
     print("- Markdown post should be copied to the posts folder")
-    print("- images should be copied to the static/images/posts/{postNameDashes} folder")
+    print(f"- images should be copied to the static/images/posts/{post_name_kebab_case} folder")
     print("")
     input("Press Enter when you have copied the post to the weblog repo...")
     
-def convert_markdown_post_to_html():
+def convert_markdown_post_to_html(post_name_kebab_case):
     os.system('clear')
     print("* STEP 3: convert Markdown post to html")
     print("")
     print("(assuming you're in the root of the weblog repo)")
-    print("COMMAND: pandoc posts/{postNameDashes}.md -o posts/{postNameDashes}.html")
+    print(f"COMMAND: pandoc posts/{post_name_kebab_case}.md -o posts/{post_name_kebab_case}.html")
+    print("")
     print("After running the command a HTML file of the Markdown post should be created in the posts folder.")
+    print("")
     input("Press Enter when you have converted the Markdown post to HTML...")
     
 def add_toc_to_html_post():
     os.system('clear')
     print("* STEP 4: add table of contents to HTML post")
     print("")
-    print("open LibreChat")
-    print("copy the HTML of the post into LibreChat")
-    print("run this prompt:")
-    print("Create a HTML table of contents for the HTML above. Returning nothing else but the HTML.")
-    print("add HTML returned by LLM after <h1> tag in HTML post")
+    print("- open LibreChat")
+    print("- copy the HTML of the post into LibreChat")
+    print("- run this prompt: Create a HTML table of contents for the HTML above. Returning nothing else but the HTML.")
+    print("- add HTML returned by LLM after <h1> tag in HTML post")
+    print("")
     input("Press Enter when you have added the table of contents to the HTML post...")
     
-def check_post_served_by_web_server():
+def check_post_served_by_web_server(post_name_kebab_case):
     os.system('clear')
     print("* STEP 5: run web server in Docker")
     print("")
     print("COMMAND: docker build -t weblog .")
     print("COMMAND: docker run -p 8000:8000 weblog")
-    print("open http://0.0.0.0:8000/posts/{postNameDashes} in your browser")
+    print("")
+    print(f"open http://0.0.0.0:8000/posts/{post_name_kebab_case} in your browser to check if the post is displayed correctly")
+    print("")
     print("CHECKLIST:")
     print("- post visible in the browser")
     print("- images displayed correctly")
     print("- table of contents displayed correctly")
     print("- no major layout issues")
-    
+    print("")
     input("Press Enter when you have run the web server and checked the post in the browser...")
 
 def main():
     # run steps in sequence
     post_names = ask_for_post_name()
     create_post_in_obsidian(post_names['kebab_case'])
-    copy_obsidian_post_to_weblog_repo()
-    convert_markdown_post_to_html()
+    copy_obsidian_post_to_weblog_repo(post_names['kebab_case'])
+    convert_markdown_post_to_html(post_names['kebab_case'])
     add_toc_to_html_post()
-    check_post_served_by_web_server()
+    check_post_served_by_web_server(post_names['kebab_case'])
     
     os.system('clear')
+    
+    print("Post has been added.")
 
 if __name__ == "__main__":
     main()
