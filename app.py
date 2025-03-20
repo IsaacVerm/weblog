4d9dc9e52b6f01d991efefeecdb61b7032b996e0
IsaacVerm <isaacverm@gmail.com>
2025-03-20 22:33:36 +0100

    add Linkfest page (#18)
    * add 5 links as links.json
    
    * serve data files (like links.json)
    
    * keep links.json in static folder
    
    * add linkfest page
    
    * fix typo
    
    * update css linkfest page
    
    * add key text to links linkfest page
    
    * make /private version weblog available
    
    * add linkfest to private weblog

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def show_index():
     return send_from_directory('static/pages', 'posts_overview_public.html')
 
+@app.route('/private')
+def show_private_index():
+    return send_from_directory('static/pages', 'posts_overview_private.html')
+
+@app.route('/linkfest')
+def show_linkfest():
+    return send_from_directory('static/pages', 'linkfest.html')
+
 @app.route('/posts/<post_name>')
 def show_post(post_name):
     # using f-string to avoid having to use the complete filename in the url
     # now you don't have to specify the .html extension in the url yourself
     # e.g. /posts/first-post instead of /posts/first-post.html
     return send_from_directory('static/posts', f'{post_name}.html')
+
+@app.route('/data/<filename>')
+def get_data(filename):
+    return send_from_directory('static/data', f'{filename}.json', mimetype='application/json')
